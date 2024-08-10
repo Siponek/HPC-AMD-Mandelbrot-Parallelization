8c94e56ee58dc7bd78de7b345b2e783b7e1d1534
siponek <szinkowicz@gmail.com>
2024-08-10 22:17:18 +0200

    fix: cudaMemcpy bug. Insufficient memory allocation for input data. Refactored code, smart ptr, and added error handling.

--- src/cuda/mandelbrot.cu
+++ src/cuda/mandelbrot.cu
@@ -7,7 +7,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <complex>
 
 #include <cuComplex.h>
 #include <cuda.h>
@@ -69,44 +68,90 @@ namespace fs = std::filesystem;
 using namespace std;
 using namespace MandelbrotSet;
 
-int getThreadsUsed(int argc, char **argv)
+__host__ int get_threads_used(int argc, char **argv)
 {
-	int threads_used = omp_get_max_threads();
+	int _threads_used = omp_get_max_threads();
 	for (int i = 1; i < argc; ++i)
 	{
 		string arg = argv[i];
 		if (arg == "-threads" && i + 1 < argc)
 		{
-			threads_used = stoi(argv[++i]);
-			if (threads_used <= 0)
+			_threads_used = stoi(argv[++i]);
+			if (_threads_used <= 0)
 			{
 				cerr << "Please specify a positive number of "
 						"threads."
 					 << endl;
 				exit(-4);
 			}
-			omp_set_num_threads(threads_used);
+			omp_set_num_threads(_threads_used);
 		}
 	}
-	return threads_used;
+	return _threads_used;
 }
 
-__device__ int devMandelbrot(int col, int row, double step,
-							 int minX, int minY, int iterations)
+__host__ void check_cuda_errors(cudaError_t err_sync,
+								cudaError_t err_async)
 {
-	// cuDoubleComplex c =
-	// 	make_cuDoubleComplex(minX + col * step, minY + row * step);
-	// cuDoubleComplex z = make_cuDoubleComplex(0, 0);
-	const complex<double> c(minX + col * step, minY + row * step);
-	complex<double> z(0, 0);
-	int count = 0;
-	while (abs(z) < 2.0 && count < iterations)
+	if (err_sync != cudaSuccess)
 	{
-		// z = cuCadd(cuCmul(z, z),
-		// 		   c);
-		z = pow(z, 2) + c;
+		cerr << "Sync kernel error: "
+			 << cudaGetErrorString(err_sync) << endl;
+		exit(-1);
+	}
+	if (err_async != cudaSuccess)
+	{
+		cerr << "Async kernel error: "
+			 << cudaGetErrorString(err_async) << endl;
+		exit(-1);
+	}
+}
+
+__host__ void log_execution_details(
+	const string &logFile, const string &file_name, int iterations,
+	int RESOLUTION_VALUE, int WIDTH, int HEIGHT, double STEP,
+	int _threads_used, chrono::duration<double> duration)
+{
+	ofstream log(logFile, ios::app);
+	if (log.is_open())
+	{
+		log << "Date:\t" << __DATE__ << " " << __TIME__
+			<< "\tProgram:\t" << file_name << "\t\tIterations:\t"
+			<< iterations << "\tResolution:\t" << RESOLUTION_VALUE
+			<< "\tWidth:\t" << WIDTH << "\tHeight:\t" << HEIGHT
+			<< "\tStep:\t" << STEP << "\tScheduling:\t"
+			<< SCHEDULING_STRING << "\tThreads:\t" << _threads_used
+			<< "\tTime:\t" << duration.count() << "\tseconds"
+			<< endl;
+		log.close();
+	}
+	else
+	{
+		cerr << "Unable to open log file." << endl;
+	}
+}
+
+__device__ int dev_Mandelbrot_kernel(int collumn, int row,
+									 double step, int minX,
+									 int minY, int iterations)
+{
+	int count = 0;
+
+	cuDoubleComplex c = make_cuDoubleComplex(minX + collumn * step,
+											 minY + row * step);
+	cuDoubleComplex z = make_cuDoubleComplex(0, 0);
+	while (cuCabs(z) < 2.0 && count < iterations)
+	{
+		z = cuCadd(cuCmul(z, z), c);
 		count++;
 	}
+	// const complex<double> c(minX + collumn * step, minY + row *
+	// step); complex<double> z(0, 0); while (abs(z) < 2.0 && count
+	// < iterations)
+	// {
+	// 	z = pow(z, 2) + c;
+	// 	count++;
+	// }
 
 	return (count < iterations) ? count : 0;
 }
@@ -115,26 +160,25 @@ __global__ void mandelbrotKernel(int *image, double step, int minX,
 								 int minY, int iterations,
 								 int WIDTH, int HEIGHT)
 {
+	// idiomatic CUDA loop
 	int col = blockIdx.x * blockDim.x + threadIdx.x;
 	int row = blockIdx.y * blockDim.y + threadIdx.y;
-
 	if (col >= WIDTH || row >= HEIGHT)
 		return;
 
 	int index = row * WIDTH + col;
-
-	image[index] =
-		devMandelbrot(col, row, step, minX, minY, iterations);
+	image[index] = dev_Mandelbrot_kernel(col, row, step, minX, minY,
+										 iterations);
 }
 
 int main(int argc, char **argv)
 {
 	// cout.sync_with_stdio(false);
-	fs::path filePath = argv[0];
-	string fileName = filePath.filename().string();
+	fs::path file_path = argv[0];
+	string file_name = file_path.filename().string();
 	if (argc < 3)
 	{
-		cout << "Usage: " << fileName
+		cout << "Usage: " << file_name
 			 << " <output_file> <iterations>" << endl;
 		return -1;
 	}
@@ -151,74 +195,76 @@ int main(int argc, char **argv)
 			 << endl;
 		return -2;
 	}
-	const int threads_used = getThreadsUsed(argc, argv);
-	int *const image = new int[HEIGHT * WIDTH];
+	const int _threads_used = get_threads_used(argc, argv);
 	const size_t image_size = HEIGHT * WIDTH;
-	// const size_t image_size = HEIGHT * WIDTH * sizeof(int);
-	fill_n(image, image_size, -1);
+	unique_ptr<int[]> image(new int[image_size]);
+
+	fill_n(image.get(), image_size, -1);
 	int *device_image;
-	// cuda::malloc((void **)&device_image, HEIGHT * WIDTH *
-	// sizeof(int));
-	cudaError_t	err = cuda::malloc((void **)&device_image, HEIGHT * WIDTH * sizeof(int));
-	
-	if (err != cudaSuccess || device_image == nullptr)
+	size_t free_mem, total_mem;
+	cudaMemGetInfo(&free_mem, &total_mem);
+
+	size_t required_mem = HEIGHT * WIDTH * sizeof(int);
+	if (required_mem > free_mem)
+	{
+		cerr << "Error: Not enough memory on the device." << endl;
+		return -1;
+	}
+	if (cuda::malloc((void **)&device_image, required_mem) !=
+		cudaSuccess)
 	{
 		cerr << "Error allocating memory on device." << endl;
 		return -33;
 	}
-	cudaMemcpy(device_image, image, image_size,
-			   cudaMemcpyHostToDevice);
-	dim3 blockSize(threads_used, threads_used);
-	dim3 gridSize((WIDTH + blockSize.x - 1) / blockSize.x,
-				  (HEIGHT + blockSize.y - 1) / blockSize.y);
 
-	cout << "Calculating Mandelbrot set with " << threads_used
-		 << " threads with " << iterations << " iterations."
-		 << endl;
+	cudaMemcpy(device_image, image.get(), image_size * sizeof(int),
+			   cudaMemcpyHostToDevice);
+	dim3 threads_per_block(16, 16);
+	dim3 blocks_per_grid(
+		(WIDTH + threads_per_block.x - 1) / threads_per_block.x,
+		(HEIGHT + threads_per_block.y - 1) / threads_per_block.y);
+
+	cout << "Image size: " << WIDTH << "x" << HEIGHT << endl;
+	cout << "Calculating Mandelbrot set with " << _threads_used
+		 << " threads with " << iterations << " iterations." << endl
+		 << "blocksize: " << blocks_per_grid.x << " "
+		 << blocks_per_grid.y
+		 << " threads_per_block: " << threads_per_block.x << " "
+		 << threads_per_block.y << endl;
 
 	const auto start = std::chrono::steady_clock::now();
-	mandelbrotKernel<<<gridSize, blockSize>>>(
+	mandelbrotKernel<<<blocks_per_grid, threads_per_block>>>(
 		device_image, STEP, MIN_X, MIN_Y, iterations, WIDTH,
 		HEIGHT);
-	cudaError_t errSync = cudaGetLastError();
-	cudaError_t errAsync = cudaDeviceSynchronize();
-	if (errSync != cudaSuccess) {
-		fprintf(stderr, "Sync kernel error: %s\n", cudaGetErrorString(errSync));
-		return -1;
-	}
-	if (errAsync != cudaSuccess) {
-		fprintf(stderr, "Async kernel error: %s\n", cudaGetErrorString(errAsync));
-		return -1;
-	}
-	cudaMemcpy(image, device_image, image_size,
+	cudaError_t err_sync = cudaGetLastError();
+	cudaError_t err_async = cudaDeviceSynchronize();
+	check_cuda_errors(cudaGetLastError(), cudaDeviceSynchronize());
+	cudaMemcpy(image.get(), device_image, image_size * sizeof(int),
 			   cudaMemcpyDeviceToHost);
+
 	const auto end = std::chrono::steady_clock::now();
+	cuda::free(device_image);
+	if (any_of(image.get(), image.get() + image_size,
+			   [](int val) { return val == -1; }))
+	{
+		cerr << "Error: Not all pixels were calculated." << endl;
+		return -3;
+	}
 
 	chrono::duration<double> duration = end - start;
 	cout << endl
 		 << "Time elapsed: " << duration.count() << " seconds."
 		 << endl;
 
-	const string logFile =
+	const string log_file =
 		logutils::createLogFileName(argv[1], "_openmp_");
-	ofstream log(logFile, ios::app);
-	if (log.is_open())
-	{
-		log << "Date:\t" << __DATE__ << " " << __TIME__
-			<< "\tProgram:\t" << fileName << "\t\tIterations:\t"
-			<< iterations << "\tResolution:\t" << RESOLUTION_VALUE
-			<< "\tWidth:\t" << WIDTH << "\tHeight:\t" << HEIGHT
-			<< "\tStep:\t" << STEP << "\tScheduling:\t"
-			<< SCHEDULING_STRING << "\tThreads:\t" << threads_used
-			<< "\tTime:\t" << duration.count() << "\tseconds"
-			<< endl;
-		log.close();
-	}
-	else
-	{
-		cerr << "Unable to open log file." << endl;
-	}
-
+	ofstream log(log_file, ios::app);
+	log_execution_details(log_file, file_name, iterations,
+						  RESOLUTION_VALUE, WIDTH, HEIGHT, STEP,
+						  _threads_used, duration);
+	//
+	//? Logging the execution details
+	//
 	const fs::path outputFilePath(argv[1]);
 	try
 	{
@@ -251,7 +297,7 @@ int main(int argc, char **argv)
 			matrix_out << endl;
 	}
 	matrix_out.close();
-
-	delete[] image; // It's here for coding style, but useless
+	image.reset(); // It's here for coding style, but useless
+	// delete[] image; // It's here for coding style, but useless
 	return 0;
 }
